7eb8816fe38414fcea0419c93267a3e574852712
shrimp.za@gmail.com <shrimp.za@gmail.com@3c5ea30b-44e6-7b04-e229-3423753af2cf>
2013-03-04 18:58:46 +0000

    Create branch for upgrading to Smarty 3

---
+++ branches/smarty3/index.php
@@ -0,0 +1,41 @@
+<?php
+    ob_start("ob_gzhandler");
+
+    $time_start = microtime(true);
+
+    require_once('includes/site.php');
+
+    $site = new Site();
+
+    if (isset($_GET['jsonMode']) || isset($_POST['jsonMode'])) {
+        $site->outputJson();
+    } else {
+        $site->output();
+    }
+
+    $time_end = microtime(true);
+    $time = $time_end - $time_start;
+
+    if ((!isset($_GET['popup']) && !isset($_GET['jsonMode'])) && $GLOBALS['config']['site']['showstats']) {
+        echo '<script type="text/javascript">document.getElementById("footer").innerHTML = document.getElementById("footer").innerHTML + "<br />';
+
+        echo '<small><small>Processed in ' . round($time, 4) . ' seconds, ';
+        echo ($site->db->numQueries + eveDB::getInstance()->db->numQueries) . ' DB queries; ';
+
+        echo apiStats::$liveRequests . ' live API requests, ' . apiStats::$cacheRequests . ' from cache; ';
+        
+        echo round((memory_get_usage()/1024), 2) . 'kb memory used.<br />';
+        
+        echo '<b>API Activity:</b><br />';
+        foreach (apiStats::$requests as $req) {
+            echo $req['cache'] ? '[Cached] ' : '';
+            echo $req['method'];
+            echo ' - ' . round($req['time'], 4) . ' secs';
+            echo ' - exp ' . date('d/n/Y H:i:s', $req['cacheUntil']) . '<br />';
+        }
+        echo '</small></small>";';
+        echo '</script>';
+    }
+
+    ob_end_flush();
+?>
